b898c686e3d4f7a3631bcff244cc3b8315be1b2f
mkl <mkl48@nau.edu>
2014-01-07 10:18:56 -0500

    updates

--- R/mgp.R
+++ R/mgp.R
@@ -1,21 +1,21 @@
-mgp <-
-function(scn,com,loc=TRUE,my.coord=''){
-e.col <- sign(scn)
-e.col[e.col==1] <- 'grey'
-e.col[e.col==-1] <- 'red'
-v.cex <- apply(com,2,sum)
-v.cex <- log(v.cex,10)
-v.cex <- v.cex * (1/min(v.cex))
-v.cex <- v.cex/2
+mgp <- function(net='species network',com='community matrix',my.coord='',loc=TRUE,v.scale=3,v.min=0.1){
+
+v.cex <- apply(com[,apply(com,2,sum)!=0],2,sum) #scaling node size by the log of species frequencies
+v.cex <- (((v.cex/sum(v.cex))/max((v.cex/sum(v.cex))))*v.scale)+v.min
+e.col <- net
+e.col[net>0.5] <- 'red'
+e.col[net<0.5] <- 'black'
+e.col[net==0.5] <- 'grey'
+
 if (length(my.coord)==1){
-  coord <- gplot(abs(scn),displaylabels=TRUE,gmode='graph',pad=1.5,
-                 edge.col=e.col,edge.lwd=abs(scn),
-                 vertex.cex=v.cex,vertex.col='darkgrey',vertex.border='darkgrey')
+  coord <- gplot(abs(net),displaylabels=FALSE,gmode='graph',pad=1.5,
+        edge.lwd=(abs(net)),vertex.cex=v.cex,vertex.col='grey',
+        edge.col=e.col)
+
 }else{
-  coord <- gplot(abs(scn),displaylabels=TRUE,gmode='graph',pad=1.5,
-                 edge.col=e.col,edge.lwd=abs(scn),
-                 vertex.cex=v.cex,vertex.col='darkgrey',vertex.border='darkgrey',
-                 coord=my.coord)
+  gplot(abs(net),displaylabels=FALSE,gmode='graph',pad=1.5,
+        edge.lwd=(abs(net)),vertex.cex=v.cex,vertex.col='grey',
+        edge.col=e.col,coord=my.coord)
 }
 if (loc){return(coord)}else{}
 }
