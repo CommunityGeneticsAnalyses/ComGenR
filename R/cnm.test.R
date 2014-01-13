a1251badf9ed54717055de48629efb31e4030c89
mkl <mkl48@nau.edu>
2014-01-13 16:22:26 -0500

    updates

--- R/cnm.test.R
+++ R/cnm.test.R
@@ -1,10 +1,16 @@
-cnm.test <- function(com,method='r1',nits=5000,burn=500,thin=10,threshold=0){
+cnm.test <- function(com,method='r1',nits=5000,burn=500,thin=10,threshold=0,null.out=FALSE){
   ###Co-occurrence Null Modeling test
   com[com<=threshold] <- 0
   com.nul <- nullCom(com,method=method,nits=nits,burn=burn,thin=thin)
   cs.nul <- unlist(pblapply(com.nul,cscore))
   cs.obs <- cscore(com)
   ses <- (cs.obs-mean(cs.nul))/sd(cs.nul)
-  return(c(SES=ses,lower.p=length(cs.nul[cs.nul<=cs.obs])/length(cs.nul),
-           upper.p=length(cs.nul[cs.nul>=cs.obs])/length(cs.nul)))
+  if (null.out){
+    stats <- c(SES=ses,lower.p=length(cs.nul[cs.nul<=cs.obs])/length(cs.nul),
+             upper.p=length(cs.nul[cs.nul>=cs.obs])/length(cs.nul))
+    return(list(sim=com.nul,stats=stats))
+  }else{
+    return(c(SES=ses,lower.p=length(cs.nul[cs.nul<=cs.obs])/length(cs.nul),
+             upper.p=length(cs.nul[cs.nul>=cs.obs])/length(cs.nul)))
+  }
 }
