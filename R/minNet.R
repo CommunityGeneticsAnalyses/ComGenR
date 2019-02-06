59772a34983cd8df08bdd2d42f44ebafc6b35cc9
mklau <matthewklau@fas.harvard.edu>
2019-02-06 16:52:59 -0500

    removes igraph dependency with update to minNet

--- R/minNet.R
+++ R/minNet.R
@@ -20,15 +20,15 @@
 
 minNet <- function(x, FUN = 'cdNet', alpha = 0.05){
     thresh <- start
-    g <- get(FUN)(m,alpha=alpha)
-    no.c <- no.clusters(graph.adjacency(g,weighted=TRUE))
+    g <- get(FUN)(m, alpha=alpha)
+    no.c <- no.clusters(igraph::graph.adjacency(g,weighted=TRUE))
     while (no.c == 1){
         thresh <- thresh + 1
         m[m < thresh] <- 0
         g <- get(FUN)(m,alpha=alpha)
-        w <- c(w,components(graph.adjacency(g,weighted=TRUE),mode='weak'))
-        s <- c(s,components(graph.adjacency(g,weighted=TRUE),mode='strong'))
-        no.c <- no.clusters(graph.adjacency(g,weighted=TRUE))
+        w <- c(w,igraph::components(igraph::graph.adjacency(g,weighted=TRUE),mode='weak'))
+        s <- c(s,igraph::components(igraph::graph.adjacency(g,weighted=TRUE),mode='strong'))
+        no.c <- no.clusters(igraph::graph.adjacency(g,weighted=TRUE))
     }
     m <- x
     m[m < (thresh - 1)] <- 0
