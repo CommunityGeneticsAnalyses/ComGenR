0069e61de1006707d8b3441e668d24e1b3630910
mklau <matthewklau@fas.harvard.edu>
2019-02-06 16:36:45 -0500

    adds dotplot

---
+++ R/mdc.plot.R
@@ -0,0 +1,53 @@
+#' Mean based dot chart with error bars.
+#' 
+#' Creates a plot of means with error bars for a categorical predictor
+#' and continuous response variable..
+#' 
+#' @param x Categorical predictor.
+#' @param y Continuous response.
+#' @param pch Shape definition.
+#' @param col Color definition.
+#' @param ylim Y-axis limits.
+#' @param xlab X-axis label.
+#' @param ylab Y-axis label.
+#' @param ord Re-ordering vector for the predictor levels.
+#' @param std LOGICAL: should y be standardized?
+#' @param add LOGICAL: should the new values be added to the current plot?
+#' @param lg Lower guide for the x-axis.
+#' @param ug Upper guide for the x-axis.
+#' @note %% ~~further notes~~
+#' @author Matthew K. Lau
+#' @seealso %% ~~objects to See Also as \code{\link{help}}, ~~~
+#' @keywords ~kwd1 ~kwd2
+#' @examples
+#' 
+#' ##---- Should be DIRECTLY executable !! ----
+#' ##-- ==>  Define data, use random,
+#' ##--	or do  help(data=index)  for the standard data sets.
+#' 
+mdc.plot <- function(x, y, pch = 19, col = 1, 
+                     ylim = c(-3, 3), xlab, ylab,
+                     ord, std = TRUE, add = FALSE, lg = 0, ug = 1){
+    if (std){y <- (y - mean(y)) / sd(y)}
+    x <- factor(x)
+    mu <- tapply(y, x, mean)
+    se <- tapply(y, x, function(x) sd(x) / sqrt(length(x)))
+    if (exists("ord")){
+        se <- se[ord]
+        mu <- mu[ord]
+    }
+    se.bars <- matrix(c(mu + se, mu - se), nrow = 2, byrow = TRUE)
+    n <- nlevels(x)
+    x.grid <- seq(lg, ug, by = 1 / (length(unique(x)) + 1))
+    if (!exists("xlab")){xlab = ""}
+    if (!exists("ylab")){ylab = ""}
+    if (!add){
+        plot(x.grid, rep(0, length(x.grid)), pch = "", 
+             ylab = ylab, xlab = xlab, xaxt = "none", ylim = ylim)
+    }
+    axis(1, at = x.grid[(1:n + 1)], labels = levels(x))
+    points(x.grid[(1:n + 1)], mu, pch = pch)
+    for (i in 1:length(mu)){
+        lines(rep(x.grid[(1:n + 1)][i], 2), c(mu[i] - se[i], mu[i] + se[i]))
+    }
+}
