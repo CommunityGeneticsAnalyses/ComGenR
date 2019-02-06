a7c75dfe1b815372f48db440eff33c5b6aee0877
mklau <matthewklau@fas.harvard.edu>
2019-02-06 16:22:48 -0500

    updates ch.plot

--- R/ch.plot.R
+++ R/ch.plot.R
@@ -10,10 +10,9 @@
 #' @param x Ordination matrix.
 #' @param g Grouping (i.e. genotype) vector.
 #' @param cex Value to scale centroids.
-#' @param plot.legend Logical: should a legend be generated?
-#' @param loc Location of the legend (e.g. top, top-right, right, bottom-right,
-#' bottom, bottom-left,left,top-left).
-#' @param mu.pch Input to define the shape of the centroids.
+#' @param mu.pch Centroid shape.
+#' @param pt.col Centroid color.
+#' @param car.col Error bar color.
 #' @note %% ~~further notes~~
 #' @author Matthew K. Lau
 #' @seealso %% ~~objects to See Also as \code{\link{help}}, ~~~
@@ -24,32 +23,35 @@
 #' ##-- ==>  Define data, use random,
 #' ##--	or do  help(data=index)  for the standard data sets.
 #' 
-ch.plot <- function(x = 'ordination matrix', g = 'groupings', cex = 1, plot.legend = FALSE, loc = 'topleft', mu.pch=19){
-  mu <- apply(x,2,function(x,g) tapply(x,g,mean),g=g)
-  se <- apply(x,2,function(x,g) tapply(x,g,function(x) sd(x)/sqrt(length(x))),g=g)
-  mu <- na.omit(mu)
-  se <- na.omit(se)
+ch.plot <- function(x = 'ordination matrix', 
+                    g = 'groupings', 
+                    cex = 1, 
+                    mu.pch = 19,
+                    pt.col = 1, 
+                    bar.col = 1){
+    mu <- apply(x, 2, function(x, g) tapply(x, g, mean), g = g) 
+    se <- apply(x, 2, function(x, g) tapply(x, g, function(x)
+            sd(x)/sqrt(length(x))), g = g) 
+    mu <- na.omit(mu) 
+    se <- na.omit(se)
                                         #error bars
-  cl.xu <- mu[,1] + se[,1]
-  cl.xl <- mu[,1] - se[,1]
-  cl.yu <- mu[,2] + se[,2]
-  cl.yl <- mu[,2] - se[,2]
-    if (plot.legend){
-                                        #coloring
-      mu.col <- rainbow(length(unique(g)))[as.numeric(unique(g))]
-      plot(mu,pch=mu.pch,cex=cex,xlim=c(min(cl.xl),max(cl.xu)),ylim=c(min(cl.yl),max(cl.yu)),col=mu.col)
-      for (i in 1:nrow(mu)){
-        lines(x=c(cl.xl[i],cl.xu[i]),y=c(mu[i,2],mu[i,2]))
-        lines(x=c(mu[i,1],mu[i,1]),y=c(cl.yl[i],cl.yu[i]))
-      }    
-      legend(loc,legend=rownames(se),cex=cex*0.5,pch=mu.pch,col=mu.col,border='grey')
-  }else{
-                                        #coloring
-    mu.col <- 'black'
-    plot(mu,pch=mu.pch,cex=cex,xlim=c(min(cl.xl),max(cl.xu)),ylim=c(min(cl.yl),max(cl.yu)),col=mu.col)
+    cl.xu <- mu[, 1] +  se[, 1]
+    cl.xl <- mu[, 1] -  se[, 1]
+    cl.yu <- mu[, 2] + se[, 2]
+    cl.yl <-  mu[, 2] -  se[, 2]
+                                        # plotting
+    plot(mu, pch = 0, cex = 0, 
+         xlim = c(min(cl.xl), max(cl.xu)), 
+         ylim = c(min(cl.yl), max(cl.yu)))
     for (i in 1:nrow(mu)){
-      lines(x=c(cl.xl[i],cl.xu[i]),y=c(mu[i,2],mu[i,2]))
-      lines(x=c(mu[i,1],mu[i,1]),y=c(cl.yl[i],cl.yu[i]))
-    }
-  }
+        lines(x = c(cl.xl[i], cl.xu[i]), 
+              y = c(mu[i, 2], mu[i, 2]), 
+              col = bar.col)
+        lines(x = c(mu[i, 1], mu[i, 1]), 
+              y = c(cl.yl[i], cl.yu[i]), 
+              col = bar.col)
+        }
+    points(mu, cex = cex, col = pt.col, pch = mu.pch)
+                                        # Return
+    mu
 }
