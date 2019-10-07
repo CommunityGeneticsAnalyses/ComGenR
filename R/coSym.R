175947457942ff74415fb309a2a6b60f5b9a6183
mklau <matthewklau@fas.harvard.edu>
2019-10-07 15:17:12 -0400

    edits coSym.R

--- R/coSym.R
+++ R/coSym.R
@@ -2,22 +2,9 @@
 #' 
 #' Calculates node level statistics describing the symmetry of species
 #' interactions in the network using the methods of Araujo et al. 2011.
-#' 
-#' %% ~~ If necessary, more details than the description above ~~
-#' 
-#' @param x %% ~~Describe \code{x} here~~
-#' @param zero.na %% ~~Describe \code{zero.na} here~~
-#' @return %% ~Describe the value returned %% If it is a LIST, use %%
-#' @note %% ~~further notes~~
-#' @author %% ~~who you are~~
-#' @seealso %% ~~objects to See Also as \code{\link{help}}, ~~~
-#' @references %% ~put references to the literature/web site here ~
-#' @keywords ~kwd1 ~kwd2
-#' @examples
-#' 
-#' ##---- Should be DIRECTLY executable !! ----
-#' ##-- ==>  Define data, use random,
-#' ##--	or do  help(data=index)  for the standard data sets.
+#' @param x Dependency network.
+#' @param zero.na LOGICAL: should zero values be removed?
+#' @return Generates a measure of co-occurrence matrix symmetry.
 
 coSym <- function(x = 'dependency network', zero.na = TRUE){
   out <- x * 0
